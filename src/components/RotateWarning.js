cbf665b95fec685d2e7c443cdfa72d4217be6800
harry21032003 <andres2024.nuevascuentas@gmail.com>
2025-07-10 12:07:47 -0500

    Create RotateWarning.js

---
+++ src/components/RotateWarning.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './RotateWarning.css'; 
+
+export default function RotateWarning() {
+  return (
+    <div className="rotate-warning">
+      <div className="rotate-icon">🔄</div>
+      <p>Gira tu celular para ver el contenido en horizontal</p>
+    </div>
+  );
+}
