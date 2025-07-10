6028fd37b4afe3a1d225e629dd1b4b063dbaec8b
harry21032003 <andres2024.nuevascuentas@gmail.com>
2025-07-10 14:22:35 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Scene from './components/scena';
-import RotateWarning from './components/RotateWarning';   // <-- importa el nuevo componente
+import RotateWarning from './components/RotateWarning';   
 import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-      <RotateWarning />      {/* <-- aquí lo insertas */}
+      <RotateWarning />   
       <Scene />
     </div>
   );
