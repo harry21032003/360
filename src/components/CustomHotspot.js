2f26e93811d1d0c3858f4e7306d48cc15c08f8e6
harry21032003 <andres2024.nuevascuentas@gmail.com>
2025-07-11 14:03:23 -0500

    Update CustomHotspot.js

--- src/components/CustomHotspot.js
+++ src/components/CustomHotspot.js
@@ -6,15 +6,12 @@ export default function CustomHotspot({ previewImage, label }) {
 
   return (
     <div
-      className={`hotSpotElementCustom ${isActive ? 'active' : ''}`}
+      className={`hotSpotOverlay ${isActive ? 'active' : ''}`}
       onMouseEnter={() => setActive(true)}
       onMouseLeave={() => setActive(false)}
       onTouchStart={() => setActive(true)}
       onTouchEnd={() => setActive(false)}
     >
-      {/* Hotspot base original */}
-      <div className="hotSpotElement"></div>
-
       {/* Imagen preview que aparece solo al hacer hover */}
       <img className="hotspot-preview" src={previewImage || ''} alt="Preview" />
 
@@ -23,5 +20,6 @@ export default function CustomHotspot({ previewImage, label }) {
     </div>
   );
 }
+
 
 
